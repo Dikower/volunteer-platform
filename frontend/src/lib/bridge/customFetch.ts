89ddc1f2f997422b8c99cdf90dbc418b2d239760
Din Dmitriy <dmitriy1d01@gmail.com>
2022-06-10 23:04:55 +0300

    Added auth

---
+++ frontend/src/lib/bridge/customFetch.ts
@@ -0,0 +1,129 @@
+import type { Writable } from "svelte/store";
+import { writable } from "svelte/store";
+
+export const apiUrl = "http://localhost:8000/";
+
+export interface FetchParams {
+  path?: string;
+  query?: any;
+  json?: any;
+  token?: string;
+  cache?: boolean;
+  store?: Writable<any>;
+}
+
+export enum Methods {
+  get = "get", post = "post", put = "put", delete = "delete"
+}
+
+export const fetches = {
+  get: (params: FetchParams): Writable<any> => {
+    const { path } = params;
+    return storeFetch({ url: new URL(path, apiUrl), method: Methods.get, ...params });
+  },
+  post: (params: FetchParams): Writable<any> => {
+    const { path } = params;
+    return storeFetch({ url: new URL(path, apiUrl), method: Methods.post, ...params });
+  },
+  put: (params: FetchParams): Writable<any> => {
+    const { path } = params;
+    return storeFetch({ url: new URL(path, apiUrl), method: Methods.put, ...params });
+  },
+  delete: (params: FetchParams): Writable<any> => {
+    const { path } = params;
+    return storeFetch({ url: new URL(path, apiUrl), method: Methods.delete, ...params });
+  }
+};
+
+export interface StoreFetchParams extends FetchParams {
+  url: URL;
+  method: Methods;
+}
+
+export interface StoreFetchResult {
+  savedResolve: undefined;
+  promise: Promise<any>;
+  data: any;
+  loading: boolean;
+  ready: boolean;
+}
+
+export function initValues(): StoreFetchResult {
+  // Save Resolve, because Promise.resolve creates new Promise, that lags with js await
+  let savedResolve;
+  const promise = new Promise((resolve) => savedResolve = resolve);
+  return { savedResolve, promise, data: undefined, loading: true, ready: false };
+}
+
+interface Request {
+  method: Methods,
+  headers: { Accept, Authorization?: string | undefined, "Content-Type" },
+  body?: string
+}
+
+function encodeQuery(obj): string {
+  const str = [];
+  for (const p in obj)
+    // eslint-disable-next-line no-prototype-builtins
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
+
+export function storeFetch(params: StoreFetchParams): Writable<StoreFetchResult> {
+  const { url, token, method, path, json = {}, store = writable(), cache = false, query = {} } = params;
+  store.set(initValues());
+  const queriedUrl = url.href + "?" + encodeQuery(query);
+  const cacheKey = JSON.stringify(params);
+  if (cache) {
+    const saved = localStorage.getItem(cacheKey);
+    const cached = saved && JSON.parse(saved);
+    if (cached) {
+      // @ts-ignore
+      console.log(`Using cache for fetch ${queriedUrl}`);
+      store.update(obj => {
+        obj.data = cached;
+        obj.ready = true;
+        return obj;
+      });
+    }
+  }
+
+  async function load() {
+    const headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    };
+    if (token) {
+      // @ts-ignore
+      headers["Authorization"] = "Bearer " + token;
+    }
+    const request: Request = {
+      method,
+      headers
+    };
+
+    if (method !== Methods.get) {
+      request.body = JSON.stringify(json);
+    }
+    const response = await fetch(queriedUrl, request);
+    const data = await response.json().catch(e => console.warn(`Exception during fetch ${path}: ${e}`));
+    if (cache) {
+      localStorage.setItem(cacheKey, JSON.stringify(data));
+    }
+    return data;
+  }
+
+  load().then(data => {
+    // @ts-ignore
+    store.update(obj => {
+      obj.data = data;
+      obj.ready = true;
+      obj.promise = obj.savedResolve(data);
+      obj.loading = false;
+      return obj;
+    });
+  });
+  return store;
+}
---
+++ frontend/src/routes/auth/signin.svelte
@@ -0,0 +1,194 @@
+<script context="module">
+  export function load({ session }) {
+    // if (session.token) {
+    //   return {redirect: '/users/self', status: 302}
+    // }
+    return {};
+  }
+</script>
+
+<script>
+  import { goto } from "$app/navigation";
+  import { bridge } from "$lib/shared";
+  import { slide } from "svelte/transition";
+  import { session } from "$app/stores";
+  import * as cookie from "$lib/cookies";
+
+  let username = "";
+  let password = "";
+  let errorMessage = null;
+  let errors = { username: false, password: false };
+  let rsp = bridge.createStore();
+
+  function validate() {
+    if (username === "") {
+      errorMessage = "Введите E-mail";
+      errors.username = true;
+      return false;
+    }
+    // if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(username)){
+    //   errorMessage = 'Введите валидный E-mail'
+    //   errors.username = true;
+    //   return false;
+    // }
+    if (password === "") {
+      errorMessage = "Введите пароль";
+      errors.password = true;
+      return false;
+    }
+    return true;
+  }
+
+  async function submit() {
+    if (!validate()) return;
+    bridge.post({ path: "users/login", json: { username, password }, store: rsp });
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const data = await $rsp.promise;
+    if (data.detail === "Incorrect username or password") {
+      errorMessage = "Указана неверная почта или пароль";
+      errors.username = true;
+      errors.password = true;
+    } else if (!data.access_token) {
+      errorMessage = "Произошла непредвиденная ошибка";
+    } else {
+      $session.token = data.access_token;
+      cookie.set("token", data.access_token);
+      goto("/users/self");
+    }
+  }
+</script>
+
+<svelte:head>
+  <title>Вход</title>
+  <link rel="icon" href="data:image/svg+xml,
+    <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+    <text y=%22.9em%22 font-size=%2290%22>🔐</text>
+    </svg>"
+  >
+</svelte:head>
+
+<div class="main-block">
+
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <img src="https://static.tildacdn.com/tild3232-6435-4164-b565-313266623131/space.svg" alt="auth" />
+  <div class="wrapper">
+    <h1>Зайти в <span class="green">пространство</span></h1>
+    <div class="form">
+
+      <input on:focus={() => errors.username = false} class:error={errors.username} bind:value={username}
+             placeholder="E-mail" type="email">
+      <input on:focus={() => errors.password = false} class:error={errors.password} bind:value={password}
+             placeholder="Пароль" type="password">
+      {#if (errorMessage != null)}
+        <h3 transition:slide|local>{errorMessage}</h3>
+      {/if}
+      <div class="buttons">
+        <button on:click={submit}>Войти</button>
+        <a href="/auth/signup">Зарегистрироваться</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .main-block {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 90vh;
+  }
+
+  img {
+    max-width: 90%;
+  }
+
+  h1 {
+    font-weight: 700;
+  }
+
+  h3 {
+    color: #E84855;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  .green {
+    color: #43DFA8;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+    max-width: 360px;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    max-width: 360px;
+    width: 100%;
+  }
+
+  input {
+    width: 90%;
+    height: 48px;
+    margin-bottom: 0.5em;
+    border-radius: 8px;
+    padding: 0 5%;
+    border: 1px solid #E1E3E6;
+    transition: all 0.6s ease;
+  }
+
+  input:focus {
+    outline: none;
+    border: 1px solid #43DFA8;
+    box-shadow: 0 0 10px rgba(67, 223, 168, 0.5);
+  }
+
+  .error {
+    border-color: #ff2121;
+    box-shadow: 0 0 10px rgb(253, 47, 47);
+  }
+
+  a {
+    padding: 0.75em 2em;
+    background: transparent;
+    border: 1px solid #131313;
+    border-radius: 5px;
+    color: #131313;
+    transition: all 0.6s ease;
+  }
+
+  a:hover {
+    outline: none;
+    text-decoration: none;
+    background-color: #43DFA8;
+    border-color: transparent;
+  }
+
+  .buttons {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 1000px) {
+    .main-block {
+      flex-direction: column;
+      height: 100%;
+    }
+
+    h1 {
+      text-align: center;
+    }
+  }
+
+  @media (max-width: 360px) {
+    button, a {
+      padding: 10px;
+      font-size: 15px;
+    }
+  }
+</style>
