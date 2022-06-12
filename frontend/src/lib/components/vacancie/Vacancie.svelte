f83da6ca60aaf3bece1b8239bc1ef5eb7ebfcc7a
kuvshinovmark <kuvshinovmark2001@gmail.com>
2022-06-12 20:56:41 +0300

    vacanciePage

--- frontend/src/lib/components/vacancie/Vacancie.svelte
+++ frontend/src/lib/components/vacancie/Vacancie.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 import { goto } from "$app/navigation";
 
+import { faSun, faMoon, faLocation } from "@fortawesome/free-solid-svg-icons";
+import Fa from "svelte-fa";
+
+
 import { onMount, createEventDispatcher } from "svelte";
 const dispatch = createEventDispatcher();
 
   export let data: any = {};
   onMount(() => {
-    // console.log(data)
+    console.log(data)
   })
   function openVacancie(event: any){
     if(event.target.id !== ""){
@@ -18,14 +22,20 @@ const dispatch = createEventDispatcher();
 </script>
 
 
-<div class="card w-10/12 mx-auto bg-base-200 mt-10 flex-row justify-between shadow hover:shadow-lg p-4" on:click={(event) => openVacancie(event)}>
+<div class="card w-10/12 mx-auto mt-10 flex-row justify-between shadow hover:shadow-lg p-4 cursor-pointer" on:click={(event) => openVacancie(event)}>
   <div class="w-full flex flex-col pt-2 ">
-    <div class="flex mb-1 ml-1">
-      <p class="font-bold mt-auto">{data.name}</p>
-      <p class="text-gray-300 ml-4 text-sm mt-auto">{data.date}</p>
-      <p class="ml-auto mr-4 text-blue-300">{data.type}</p>
+    <div class="flex ml-1 ">
+      <p class="font-bold my-auto">{data.name} &nbsp</p>
+      <!-- <p class="my-auto text-xs"> - {data.type}</p> -->
+      <!-- <p class="ml-auto mr-4 text-blue-300">{data.participation_type}</p> -->
     </div>
-  
+    <div class="flex mb-1 ml-1">
+      {#if data.location !== ""}
+        <p class="text-xs my-auto mr-2 flex"><Fa icon={faLocation} size="lg" class="mr-1"/>{data.location}</p>
+      {/if}
+      <p class="text-gray-300 text-sm mt-auto my-auto">{data.date}</p>
+    </div>
+    
 
     <p class="text-sm ml-1">{data.description}</p>
 
--- frontend/src/routes/vacancies/[id].svelte
+++ frontend/src/routes/vacancies/[id].svelte
@@ -1,17 +1,76 @@
 <script lang="ts">
-  import Vacancie from "$lib/components/vacancie/Vacancie.svelte";
+  import { faSun, faMoon, faLocation } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
   let data = {
-    id: 3,
-    name: "Кормежка бродячих собак",
-    description: "Необходимо покормить бродячих собак в районе Одинцово.",
-    date: "12.02.2021",
-    type: "ofline",
-    image: "https://www.ptichka.ru/data/cache/2017dec/06/12/40990_15810-600x0.jpg",
-    skills: ["Собаки", "Забота о ближних", "Свежий воздух"]
-  };
+      id: 3,
+      name: "Кормежка бродячих собак",
+      description: "Необходимо покормить бродячих собак в районе Одинцово.",
+      date: "2022-12-21",
+      location: "Одинцово ул. Лесная д. 42",
+      type: "Стажировка",
+      tasks: "Участие в разработке стратегии привлечения средств от компаний Мониторинг рынка и поиск потенциальных партнеров, в т.ч. «холодный» поиск, формирование отношений с нуля, привлечение, формирование базы Подготовка предложения для потенциальных корпоративных партнеров Ведение переговоров с существующими и потенциальными компаниями-партнерами, подготовка и передача партнерам информационных материалов и документов, необходимых для оформления сотрудничества Ответственность за подписание и переподписание соглашений с партнерамиКоординация маркетинговых проектов c существующими компаниями-партнерами Участие в подготовке благотворительных и корпоративных мероприятий компаний-партнеров Совершенствование имеющихся инструментов привлечения корпоративной поддержки (спонсорские пакеты, благотворительные открытки и др.), разработка новых Координация работы подрядчиков (агентства, сторонние специалисты, волонтеры др.) в рамках проектов и акций, направленных на работу с корпоративными партнерами Мониторинг рынка на предмет появления новых трендов и технологий в привлечении корпоративных доноров, представление дайджеста руководству с описанием возможностей реализации в фонде.",
+      participants: 12,
+      participation_type: "ofline",
+      company: {name: "БФ Кораблик", description: "детский благотворительный фонд", rating: "5.0", grade: "1 место", img: "https://media-exp1.licdn.com/dms/image/C4D0BAQG6MLcBzJThug/company-logo_200_200/0/1519889862676?e=2147483647&v=beta&t=De-zc0-89y0BlNZSiD0S7adA43I35nsflrZSR4blNlU"},
+      image: "https://www.ptichka.ru/data/cache/2017dec/06/12/40990_15810-600x0.jpg",
+      skills: ["Собаки", "Забота о ближних", "Свежий воздух"]
+    }
 </script>
 
 
-<Vacancie {data}/>
+
+<div class="shadow mx-auto w-11/12 mt-12 p-8 flex rounded-lg">
+  <div class="mr-8 flex flex-col w-96">
+    <img class="w-80 h-80 rounded-lg" src={data.image} alt="">
+    <div class="w-80 border-2 border-gray-200 mt-4 rounded-lg p-1 flex">
+      <img src={data.company.img} alt="" class="w-16 h-16 rounded-full my-auto shadow mx-2">
+      <div>
+        <p class="text-sm mt-2">{data.company.name}</p>
+        <p class="text-xs text-gray-400 my-1">{data.company.description}</p>
+        <div class="flex text-xs">
+          <div class="flex flex-col">
+            <p class="mx-auto text-green-500">{data.company.rating}</p>
+            <p class="-mt-0.5 text-gray-400">оценка</p>
+          </div>
+          <div class="flex flex-col ml-2">
+            <p class="mx-auto text-red-500">{data.company.grade}</p>
+            <p class="-mt-0.5 text-gray-400">рейтинг</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="w-full flex flex-col rounded-lg pl-1">
+    <div class="flex">
+      <p class="font-bold mt-auto">{data.name}</p>
+      
+      <p class="text-gray-300 text-sm ml-auto">{data.date}</p>
+    </div>
+    <div class="flex mb-1">
+      {#if data.location !== ""}
+        <p class="text-xs my-auto mr-2 flex"><Fa icon={faLocation} size="lg" class="mr-1"/>{data.location}</p>
+      {/if}
+
+      <p class="ml-auto text-blue-300">{data.participation_type}</p>
+      
+    </div>
+    
+
+    <p class="text-sm my-1">{data.description}</p>
+
+    <div class="my-1">
+      <p>Задачи</p>
+      <p class="text-sm text-gray-500">{data.tasks}</p>
+    </div>
+    
+    <div class="mt-auto">
+      {#each data.skills as skill}
+        <button class="btn btn-outline btn-warning btn-xs m-1" id={skill}>{skill}</button>
+      {/each}
+    </div>
+  </div>
+</div>
+
 
 
